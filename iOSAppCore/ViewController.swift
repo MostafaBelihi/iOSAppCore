d4889db0a607ddf9842dc926bf7a983edafeb97f
Mostafa AlBelliehy <mst.dev@gmail.com>
2020-05-11 13:59:10 +0200

    Init

---
+++ iOSAppCore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAppCore
+//
+//  Created by Mostafa AlBelliehy on 11/05/2020.
+//  Copyright © 2020 Mostafa AlBelliehy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
